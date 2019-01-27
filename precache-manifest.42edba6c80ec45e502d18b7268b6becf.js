70389cdf248d6acafe44832a5429ada31772b1cc
John Cameron <john.n.cameron@gmail.com>
2019-01-27 22:22:57 +1100

    Updates

---
+++ precache-manifest.42edba6c80ec45e502d18b7268b6becf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b80eca797cb7f63f6f876f3e3fedb3f6",
+    "url": "/john-portfolio/static/media/seeYouThere.b80eca79.png"
+  },
+  {
+    "revision": "67f39a3af292ce82afd7",
+    "url": "/john-portfolio/static/css/main.7defe8e0.chunk.css"
+  },
+  {
+    "revision": "284cc4b208593f70f785",
+    "url": "/john-portfolio/static/js/1.284cc4b2.chunk.js"
+  },
+  {
+    "revision": "334d84eda1c2e782ff53",
+    "url": "/john-portfolio/static/js/runtime~main.334d84ed.js"
+  },
+  {
+    "revision": "843a44978a4836508d826e6cbeab642e",
+    "url": "/john-portfolio/static/media/checkers.843a4497.png"
+  },
+  {
+    "revision": "f2085a407f9081b47cd0bbf25dd06623",
+    "url": "/john-portfolio/static/media/colorNotes.f2085a40.png"
+  },
+  {
+    "revision": "67f39a3af292ce82afd7",
+    "url": "/john-portfolio/static/js/main.67f39a3a.chunk.js"
+  },
+  {
+    "revision": "67ab8d2759f28c3bfb2e802866fc97d0",
+    "url": "/john-portfolio/static/media/greatExpense.67ab8d27.png"
+  },
+  {
+    "revision": "8e35d0396e256f216bafa71d491280e1",
+    "url": "/john-portfolio/static/media/face-recogniser.8e35d039.png"
+  },
+  {
+    "revision": "b2847258cc9c185ef54170fe7230782b",
+    "url": "/john-portfolio/static/media/roboFriends.b2847258.png"
+  },
+  {
+    "revision": "856df2236c56c544a51a257b0cbd2a67",
+    "url": "/john-portfolio/static/media/background.856df223.jpg"
+  },
+  {
+    "revision": "8cd98bea8e4e6f068d247254143a00f4",
+    "url": "/john-portfolio/static/media/portrait.8cd98bea.jpg"
+  },
+  {
+    "revision": "6ae2abb7c8953bc99d2ae706f284ffb5",
+    "url": "/john-portfolio/index.html"
+  }
+];
