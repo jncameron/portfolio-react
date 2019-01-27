02351423c4c4f81e84e1afa5f3cefa2655bb8d52
John Cameron <john.n.cameron@gmail.com>
2019-01-27 16:25:46 +1100

    project modals and links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {Card, Image, Icon} from 'semantic-ui-react';
+import {Card, Image, Modal, Button} from 'semantic-ui-react';
 import checkers from '../assets/img/checkers.png';
 import colorNotes from '../assets/img/colorNotes.png';
 import seeYouThere from '../assets/img/seeYouThere.png';
 import greatExpense from '../assets/img/greatExpense.png';
+import faceRecogniser from '../assets/img/face-recogniser.png';
+import roboFriends from '../assets/img/roboFriends.png';
 import './Projects.css';
 
 const Projects = () => {
@@ -18,20 +20,48 @@ const Projects = () => {
 
       <ul className="grid">
         <li className="small p1">
-          <Card>
-            <Image className="project-images" src={checkers} alt="checkers"/>
-            <Card.Content>
-              <Card.Header>Checkers</Card.Header>
-              <Card.Description>Play your friends online, or against the Computer</Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              VueJs Express Node MongoDB
-            </Card.Content>
-          </Card>
+        <Modal trigger={<img className="project-images" src={checkers} alt="checkers"/>} closeIcon>
+            <Modal.Header className="ui center aligned">Checkers</Modal.Header>
+            <Modal.Content image>
+              <Image wrapped  src={checkers} />
+              <Modal.Description>
+                <p>This was my major university project. Checkers allows users to authenticate, 
+                  and play against their friends or against the computer. Users can challenge any online player
+                  and chat with opponents ingame </p>
+                <p>Technologies used include VueJs, Node, Express, MongoDB, WebSocket and a whole lot of svg manipulation.</p>
+                <Button primary>
+                  <a href="https://github.com/jncameron/checkers" target="_blank">Code</a> 
+                </Button>
+                <Button secondary>
+                <a href="http://checkers.ap-southeast-2.elasticbeanstalk.com/" target="_blank">Live Site</a> 
+                </Button>
+              </Modal.Description>
+            </Modal.Content>
+          </Modal>
+            
 
         </li>
         <li className="small p2">
-          <img className="project-images" src={colorNotes} alt="colorNotes"/>
+          <Modal trigger={<img className="project-images" src={colorNotes} alt="colorNotes" />} closeIcon>
+            <Modal.Header className="ui center aligned">Color Notes</Modal.Header>
+            <Modal.Content image>
+              <Image wrapped  src={colorNotes} />
+              <Modal.Description>
+                <p>A fun colorful place to organize your notes and get things done. 
+                  Built on React and using some great libraries, including Semantic UI React and
+                  React Tilt </p>
+                <p>Coming soon - I will build the backend using Node, Express and PostgreSQL, 
+                  so notes can be saved and accessed later.</p>
+                <Button primary>
+                  <a href="https://github.com/jncameron/colorNotes" target="_blank">Code</a> 
+                </Button>
+                <Button secondary>
+                <a href="https://github.com/jncameron/colorNotes" target="_blank">Live</a> 
+                </Button>
+              </Modal.Description>
+            </Modal.Content>
+          </Modal>
+          
         </li>
         <li className="small p3">
           <img className="project-images" src={seeYouThere} alt="seeYouThere"/>
@@ -40,10 +70,10 @@ const Projects = () => {
           <img className="project-images" src={greatExpense} alt="greatExpense"/>
         </li>
         <li className="small p2">
-          <img className="project-images" src={colorNotes} alt="colorNotes"/>
+          <img className="project-images" src={faceRecogniser} alt="faceRecogniser"/>
         </li>
         <li className="small p3">
-          <img className="project-images" src={seeYouThere} alt="seeYouThere"/>
+          <img className="project-images" src={roboFriends} alt="roboFriends"/>
         </li>
       </ul>
     </ section>
