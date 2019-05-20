acfbd7f4b838d0939da2737159098fd2901c3f3b
John Cameron <john.n.cameron@gmail.com>
2019-05-20 10:55:17 +1000

    updated projects and contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Card } from 'semantic-ui-react'; 
-import './Contact.css';
+import React from "react";
+import { Card } from "semantic-ui-react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
@@ -8,34 +8,34 @@ const Contact = () => {
       <h3 className="title">Contact John</h3>
       <Card.Group>
         <Card fluid color="green">
-        <Card.Content>
-          <Card.Header>Email</Card.Header>
-          <Card.Description>
-            <strong>john.n.cameron@gmail.com</strong>
-          </Card.Description>
-        </Card.Content>
-      </Card>
+          <Card.Content>
+            <Card.Header>Email</Card.Header>
+            <Card.Description>
+              <strong>john.n.cameron@gmail.com</strong>
+            </Card.Description>
+          </Card.Content>
+        </Card>
 
         <Card fluid color="green">
-      <Card.Content>
-        <Card.Header>Phone</Card.Header>
-        <Card.Description>
-          <strong>0435 865 772</strong>
-        </Card.Description>
-      </Card.Content>
-    </Card>
+          <Card.Content>
+            <Card.Header>Phone</Card.Header>
+            <Card.Description>
+              <strong>0435 865 772</strong>
+            </Card.Description>
+          </Card.Content>
+        </Card>
 
         <Card fluid color="green">
-      <Card.Content>
-        <Card.Header>Location</Card.Header>
-        <Card.Description><strong>Homebush NSW 2140</strong></Card.Description>
-      </Card.Content>
-    </Card>
-
-  </Card.Group>
-
+          <Card.Content>
+            <Card.Header>Location</Card.Header>
+            <Card.Description>
+              <strong>Sydney Olympic Park NSW 2127</strong>
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      </Card.Group>
     </section>
-  )
-}
+  );
+};
 
 export default Contact;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {Image, Modal, Button} from 'semantic-ui-react';
-import checkers from '../assets/img/checkers.png';
-import colorNotes from '../assets/img/colorNotes.png';
-import seeYouThere from '../assets/img/seeYouThere.png';
-import greatExpense from '../assets/img/greatExpense.png';
-import faceRecogniser from '../assets/img/face-recogniser.png';
-import roboFriends from '../assets/img/roboFriends.png';
-import './Projects.css';
+import React from "react";
+import { Image, Modal, Button } from "semantic-ui-react";
+import checkers from "../assets/img/checkers.png";
+import colorNotes from "../assets/img/colornotesv3.png";
+import seeYouThere from "../assets/img/seeYouThere.png";
+import greatExpense from "../assets/img/greatExpense.png";
+import faceRecogniser from "../assets/img/face-recogniser.png";
+import roboFriends from "../assets/img/roboFriends.png";
+import "./Projects.css";
 
 const Projects = () => {
   return (
@@ -14,75 +14,38 @@ const Projects = () => {
       <h3 className="title">My Projects</h3>
 
       <ul className="grid">
-        <li className="small p1">
-        <Modal trigger={<img className="project-images" src={checkers} alt="checkers"/>} closeIcon>
-            <Modal.Header className="ui center aligned">Checkers</Modal.Header>
-            <Modal.Content image>
-              <Image wrapped  src={checkers} />
-              <Modal.Description>
-                <p>This was my major university project. Checkers allows users to authenticate
-                  and play against their friends or against the computer. Users can challenge any online player
-                  and chat with opponents in-game </p>
-                <p>Technologies used include VueJs, Node, Express, MongoDB, WebSocket and a whole lot of svg.</p>
-                <div className="buttons">
-                  <form action="https://github.com/jncameron/checkers">
-                    <Button primary>
-                      Code 
-                    </Button>
-                  </form>
-                  <form action="http://checkers.ap-southeast-2.elasticbeanstalk.com/">
-                    <Button secondary>
-                      Live
-                    </Button>
-                  </form>
-                </div>
-              </Modal.Description>
-            </Modal.Content>
-          </Modal>
-        </li>
         <li className="small p2">
-          <Modal trigger={<img className="project-images" src={colorNotes} alt="colorNotes" />} closeIcon>
-            <Modal.Header className="ui center aligned">Color Notes</Modal.Header>
+          <Modal
+            trigger={
+              <img
+                className="project-images"
+                src={colorNotes}
+                alt="colorNotes"
+              />
+            }
+            closeIcon
+          >
+            <Modal.Header className="ui center aligned">
+              Color Notes
+            </Modal.Header>
             <Modal.Content image>
-              <Image wrapped  src={colorNotes} />
+              <Image wrapped src={colorNotes} />
               <Modal.Description>
-                <p>A fun colorful place to organize your notes and get things done. 
-                  Built on React and using some great libraries, including Semantic UI React and
-                  React Tilt </p>
-                <p>Coming soon - A back-end using Node, Express and PostgreSQL, 
-                  so notes can be saved and accessed later.</p>
-                  <div className="buttons">
+                <p>
+                  A fun colorful place to organize your notes and get things
+                  done. Built on React using some great libraries, including
+                  Particles JS and Semantic UI React.
+                </p>
+                <p>
+                  Backend written using Express and MongoDB. Users can log in to
+                  save and update notes.
+                </p>
+                <div className="buttons">
                   <form action="https://github.com/jncameron/colorNotes">
-                    <Button primary>
-                      Code 
-                    </Button>
+                    <Button primary>Code</Button>
                   </form>
                   <form action="https://jncameron.github.io/colorNotes/">
-                    <Button secondary>
-                      Live
-                    </Button>
-                  </form>
-                </div>
-              </Modal.Description>
-            </Modal.Content>
-          </Modal>
-          
-        </li>
-        <li className="small p3">
-          <Modal trigger={<img className="project-images" src={seeYouThere} alt="seeYouThere"/>} closeIcon>
-            <Modal.Header className="ui center aligned">See You There</Modal.Header>
-            <Modal.Content image>
-              <Image wrapped  src={seeYouThere} />
-              <Modal.Description>
-                <p>Another major university project. See You There is an events based android application,
-                  designed to assist international students meet and find interestiing things to do.
-                </p>
-                <p>Written in Kotlin and built with Android Studio and Firebase.</p>
-                <div className="buttons">
-                  <form action="https://github.com/jncameron/kotlin-app-see-you-there">
-                    <Button primary>
-                      Code 
-                    </Button>
+                    <Button secondary>Live</Button>
                   </form>
                 </div>
               </Modal.Description>
@@ -90,27 +53,103 @@ const Projects = () => {
           </Modal>
         </li>
         <li className="small p1">
-        <Modal trigger={<img className="project-images" src={greatExpense} alt="greatExpense"/>} closeIcon>
-            <Modal.Header className="ui center aligned">Great Expense</Modal.Header>
+          <Modal
+            trigger={
+              <img className="project-images" src={checkers} alt="checkers" />
+            }
+            closeIcon
+          >
+            <Modal.Header className="ui center aligned">Checkers</Modal.Header>
             <Modal.Content image>
-              <Image wrapped  src={greatExpense} />
+              <Image wrapped src={checkers} />
               <Modal.Description>
-                <p>A big thanks to Udemy Instructor Andrew Mead for getting me started on this one.
-                  Current functionality allows authenticating through Google, creating expenses, and viewing filtered
-                  and sorted lists of expenses in the dashboard.
+                <p>
+                  This was my major university project. Checkers allows users to
+                  authenticate and play against their friends or against the
+                  computer. Users can challenge any online player and chat with
+                  opponents in-game{" "}
                 </p>
-                <p>Future functionality will include an income section and other budgeting enhancements.</p>
+                <p>
+                  Technologies used include VueJs, Node, Express, MongoDB,
+                  WebSocket and a whole lot of svg.
+                </p>
+                <div className="buttons">
+                  <form action="https://github.com/jncameron/checkers">
+                    <Button primary>Code</Button>
+                  </form>
+                  <form action="http://checkers.ap-southeast-2.elasticbeanstalk.com/">
+                    <Button secondary>Live</Button>
+                  </form>
+                </div>
+              </Modal.Description>
+            </Modal.Content>
+          </Modal>
+        </li>
+        <li className="small p3">
+          <Modal
+            trigger={
+              <img
+                className="project-images"
+                src={seeYouThere}
+                alt="seeYouThere"
+              />
+            }
+            closeIcon
+          >
+            <Modal.Header className="ui center aligned">
+              See You There
+            </Modal.Header>
+            <Modal.Content image>
+              <Image wrapped src={seeYouThere} />
+              <Modal.Description>
+                <p>
+                  Another major university project. See You There is an events
+                  based android application, designed to assist international
+                  students meet and find interestiing things to do.
+                </p>
+                <p>
+                  Written in Kotlin and built with Android Studio and Firebase.
+                </p>
+                <div className="buttons">
+                  <form action="https://github.com/jncameron/kotlin-app-see-you-there">
+                    <Button primary>Code</Button>
+                  </form>
+                </div>
+              </Modal.Description>
+            </Modal.Content>
+          </Modal>
+        </li>
+        <li className="small p1">
+          <Modal
+            trigger={
+              <img
+                className="project-images"
+                src={greatExpense}
+                alt="greatExpense"
+              />
+            }
+            closeIcon
+          >
+            <Modal.Header className="ui center aligned">
+              Great Expense
+            </Modal.Header>
+            <Modal.Content image>
+              <Image wrapped src={greatExpense} />
+              <Modal.Description>
+                <p>
+                  A big thanks to Udemy Instructor Andrew Mead for getting me
+                  started on this one. Current functionality allows
+                  authentication through Google, Facebook or Github. Users can
+                  create and categorize expenses, and filter and sort lists of
+                  expenses in the dashboard.
+                </p>
                 <p>Built using React, Redux and Firebase for persistance.</p>
                 <div className="buttons">
                   <form action="https://github.com/jncameron/greatExpense">
-                    <Button primary>
-                      Code 
-                    </Button>
+                    <Button primary>Code</Button>
                   </form>
                   <form action="https://great-expense.firebaseapp.com">
-                    <Button secondary>
-                      Live
-                    </Button>
+                    <Button secondary>Live</Button>
                   </form>
                 </div>
               </Modal.Description>
@@ -118,26 +157,37 @@ const Projects = () => {
           </Modal>
         </li>
         <li className="small p2">
-        <Modal trigger={<img className="project-images" src={faceRecogniser} alt="faceRecogniser"/>} closeIcon>
-            <Modal.Header className="ui center aligned">Recogniser</Modal.Header>
+          <Modal
+            trigger={
+              <img
+                className="project-images"
+                src={faceRecogniser}
+                alt="faceRecogniser"
+              />
+            }
+            closeIcon
+          >
+            <Modal.Header className="ui center aligned">
+              Recogniser
+            </Modal.Header>
             <Modal.Content image>
-              <Image wrapped  src={faceRecogniser} />
+              <Image wrapped src={faceRecogniser} />
               <Modal.Description>
-                <p>Just a bit of fun with Clarifai's face detect api. 
-                  Enter an image url, the mighty brain will find faces in the crowd
+                <p>
+                  Just a bit of fun with Clarifai's face detect api. Enter an
+                  image url, the mighty brain will find faces in the crowd
                 </p>
-                <p>Built with React, with a little help from Particles.js-React and Clarifai</p>
+                <p>
+                  Built with React, with a little help from Particles.js-React
+                  and Clarifai
+                </p>
 
                 <div className="buttons">
                   <form action="https://github.com/jncameron/Recogniser">
-                    <Button primary>
-                      Code 
-                    </Button>
+                    <Button primary>Code</Button>
                   </form>
                   <form action="https://jncameron.github.io/Recogniser/">
-                    <Button secondary>
-                      Live
-                    </Button>
+                    <Button secondary>Live</Button>
                   </form>
                 </div>
               </Modal.Description>
@@ -145,27 +195,35 @@ const Projects = () => {
           </Modal>
         </li>
         <li className="small p3">
-        <Modal trigger={<img className="project-images" src={roboFriends} alt="roboFriends"/>} closeIcon>
-            <Modal.Header className="ui center aligned">RoboFriends</Modal.Header>
+          <Modal
+            trigger={
+              <img
+                className="project-images"
+                src={roboFriends}
+                alt="roboFriends"
+              />
+            }
+            closeIcon
+          >
+            <Modal.Header className="ui center aligned">
+              RoboFriends
+            </Modal.Header>
             <Modal.Content image>
-              <Image wrapped  src={roboFriends} />
+              <Image wrapped src={roboFriends} />
               <Modal.Description>
-                <p>A great little project from Andrei Neagoie's Udemy tutorials.
-                  Everyone needs a robofriend, right? Robofriends calls a list of test users from one api 
-                  and gives them random robot avatars from another.
-
+                <p>
+                  A great little project from Andrei Neagoie's Udemy tutorials.
+                  Everyone needs a robofriend, right? Robofriends calls a list
+                  of test users from one api and gives them random robot avatars
+                  from another.
                 </p>
                 <p>Built with React and a splash of Redux.</p>
                 <div className="buttons">
                   <form action="https://github.com/jncameron/robofriends">
-                    <Button primary>
-                      Code 
-                    </Button>
+                    <Button primary>Code</Button>
                   </form>
                   <form action="https://jncameron.github.io/robofriends/">
-                    <Button secondary>
-                      Live
-                    </Button>
+                    <Button secondary>Live</Button>
                   </form>
                 </div>
               </Modal.Description>
@@ -173,8 +231,8 @@ const Projects = () => {
           </Modal>
         </li>
       </ul>
-    </ section>
-  )
-}
+    </section>
+  );
+};
 
 export default Projects;
